31a21a13ba038e2e76d1468c984743fdceccd51a
magnetar-star <matthew.lvk@gmail.com>
2025-05-18 22:21:07 +0200

    backtesting done

--- backtesting.py
+++ backtesting.py
@@ -53,10 +53,49 @@ class BacktestXGB(BacktestParent):
     
 
 @dataclass
-class BacktestProphet:
-    cleaned_merged_data: pd.DataFrame
-    short_split_date: datetime
-    n_backward: int = 60
-    forecast_window: int = 24
-    forecast_metric: dict[str, list] | None = None
-    short_xgb_obj: XGBWrapper | None = None
+class BacktestProphet(BacktestParent):
+    prophet_model_obj: ProphetWrapper | None = None
+
+    def __post_init__(self):
+        start_slice = self.short_split_date - timedelta(days=self.n_backward)
+        end_slice = self.short_split_date + timedelta(hours=self.forecast_window)
+        print("Running XGBoost: starting at", start_slice)
+        print(f"ending at {end_slice}")
+
+        self.prophet_model_obj = ProphetWrapper(
+                self.cleaned_merged_data.copy(),
+                self.short_split_date,
+                start_date_slice=start_slice,
+                end_date_slice=end_slice
+                )
+        self.prophet_model_obj.run_prophet()
+        self.prophet_model_obj.get_forecast()
+    
+    def get_windowed_rsme_mae(self) -> tuple[float, float]:
+        rmse, mae = self.prophet_model_obj.get_rmse_mae()
+        return rmse, mae
+
+
+@dataclass
+class BacktestARIMA(BacktestParent):
+    arima_model_obj: ARIMAWrapper | None = None
+
+    def __post_init__(self):
+        start_slice = self.short_split_date - timedelta(days=self.n_backward)
+        end_slice = self.short_split_date + timedelta(hours=self.forecast_window)
+        print("Running XGBoost: starting at", start_slice)
+        print(f"ending at {end_slice}")
+
+        self.arima_model_obj = ARIMAWrapper(
+                self.cleaned_merged_data.copy(),
+                self.short_split_date,
+                start_date_slice=start_slice,
+                end_date_slice=end_slice
+                )
+        self.arima_model_obj.run_ARIMA(3, 1, 4)
+        self.arima_model_obj.get_forecast()
+    
+    def get_windowed_rsme_mae(self) -> tuple[float, float]:
+        rmse, mae = self.arima_model_obj.get_rmse_mae()
+        return rmse, mae
+    
